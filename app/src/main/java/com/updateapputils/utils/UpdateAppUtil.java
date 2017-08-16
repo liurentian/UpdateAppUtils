c751677a8d2f280edcea65f22b884034e3186675
liurentian <972377892@qq.com>
2017-08-16 11:17:12 +0800

    finish the first function

--- app/src/main/java/com/updateapputils/utils/UpdateAppUtil.java
+++ app/src/main/java/com/updateapputils/utils/UpdateAppUtil.java
@@ -42,7 +42,7 @@ public class UpdateAppUtil {
         getLocalVersion(activity);
     }
 
-    public UpdateAppUtil from(Activity activity){
+    public static UpdateAppUtil from(Activity activity){
         return new UpdateAppUtil(activity);
     }
 
